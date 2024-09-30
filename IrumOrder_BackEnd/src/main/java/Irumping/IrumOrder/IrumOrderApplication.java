5ab3f0809f92a163a4b871dca433ad2c633042a5
ej9374 <ej9374@naver.com>
2024-09-30 23:35:16 +0900

    backend create

---
+++ IrumOrder_BackEnd/src/main/java/Irumping/IrumOrder/IrumOrderApplication.java
@@ -0,0 +1,13 @@
+package Irumping.IrumOrder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrumOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IrumOrderApplication.class, args);
+	}
+
+}
