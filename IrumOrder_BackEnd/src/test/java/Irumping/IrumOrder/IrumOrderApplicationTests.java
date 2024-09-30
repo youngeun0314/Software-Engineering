80ac25ff22ccc1e551cc1807d2e1046db5ce0bdc
ej9374 <ej9374@naver.com>
2024-09-30 23:35:16 +0900

    backend create

---
+++ IrumOrder_BackEnd/src/test/java/Irumping/IrumOrder/IrumOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package Irumping.IrumOrder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IrumOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
