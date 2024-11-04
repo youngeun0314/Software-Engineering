03848c43bcae9e4a13eb49d3e63f90ff9d1d0f50
ye0314 <117622694+youngeun0314@users.noreply.github.com>
2024-11-04 19:29:00 +0900

    폴더 생성

---
+++ IrumOrder_BackEnd/src/main/java/Irumping/IrumOrder/Repository/MockLoginRepository.java
@@ -0,0 +1,25 @@
+package Irumping.IrumOrder.Repository;
+
+import Irumping.IrumOrder.Entity.UserEntity;
+import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Repository
+public class MockLoginRepository implements UserRepository {
+
+    private Map<String, UserEntity> mockDB = new HashMap<>();
+
+    @Override
+    public void save(String id, String password, String email, String name) {
+        mockDB.put(id, new UserEntity(id, password, email, name));
+    }
+
+    @Override
+    public String Password(String id) {
+        return mockDB.get(id).getPassword();
+    }
+
+
+}
