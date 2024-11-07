23d47551858694cc830f1c4b1bbfcbce1d6602d0
nyungovo <nyungovo@gmail.com>
2024-11-07 23:24:02 +0900

    feat: addRoutine
    아직 틀 잡는중
    
    issue #5

--- IrumOrder_BackEnd/src/main/java/Irumping/IrumOrder/Entity/RoutineEntity.java
+++ IrumOrder_BackEnd/src/main/java/Irumping/IrumOrder/Entity/RoutineEntity.java
@@ -1,7 +1,15 @@
 package Irumping.IrumOrder.Entity;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import nonapi.io.github.classgraph.json.Id;
+
 public class RoutineEntity {
+
     private String userID;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int routineID;
     private String routineDay;
     //private - time;
     private boolean offAlarm;
