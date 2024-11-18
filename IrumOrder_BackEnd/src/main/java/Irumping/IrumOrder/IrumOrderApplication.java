d2e447ee20c992af889f82560f3fcd83a7fdc125
ej9374 <ej9374@naver.com>
2024-11-18 17:24:32 +0900

    chore: 클래스 생성

--- IrumOrder_BackEnd/src/main/java/Irumping/IrumOrder/IrumOrderApplication.java
+++ IrumOrder_BackEnd/src/main/java/Irumping/IrumOrder/IrumOrderApplication.java
@@ -2,12 +2,12 @@ package Irumping.IrumOrder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class IrumOrderApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(IrumOrderApplication.class, args);
 	}
-
 }
