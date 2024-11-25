57d95590a85f8e79c4e6c9c3bd385ba376acf0d2
nyungovo <nyungovo@gmail.com>
2024-11-26 02:15:41 +0900

    chore: unused import statement 삭제
    RoutineService unused import statement 삭제
    
    issue #5

--- IrumOrder_BackEnd/src/main/java/Irumping/IrumOrder/service/RoutineService.java
+++ IrumOrder_BackEnd/src/main/java/Irumping/IrumOrder/service/RoutineService.java
@@ -1,7 +1,6 @@
 package Irumping.IrumOrder.service;
 
 import Irumping.IrumOrder.dto.RoutineDto;
-import Irumping.IrumOrder.entity.RoutineDay;
 import Irumping.IrumOrder.entity.RoutineEntity;
 import Irumping.IrumOrder.exeption.UserIdMismatchException;
 import Irumping.IrumOrder.repository.RoutineRepository;
@@ -9,7 +8,6 @@ import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalTime;
 import java.util.List;
 
 
