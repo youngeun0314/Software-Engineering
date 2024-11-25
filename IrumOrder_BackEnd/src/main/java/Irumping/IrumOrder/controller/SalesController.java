07a1d1c6cbdf58bd4f3fa16d6ef77e93ccb2ad62
nyungovo <nyungovo@gmail.com>
2024-11-25 13:53:26 +0900

    chore: 주석 삭제
    SalesController 주석 삭제
    
    issue #6

--- IrumOrder_BackEnd/src/main/java/Irumping/IrumOrder/controller/SalesController.java
+++ IrumOrder_BackEnd/src/main/java/Irumping/IrumOrder/controller/SalesController.java
@@ -2,6 +2,9 @@ package Irumping.IrumOrder.controller;
 
 import Irumping.IrumOrder.dto.SalesResponse;
 import Irumping.IrumOrder.service.SalesService;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +15,13 @@ public class SalesController {
     @Autowired
     private SalesService salesService;
 
+//    @ApiResponse(
+//            description = "Sales response",
+//            content = @Content(
+//                    mediaType = "application/json",
+//                    examples = @ExampleObject(value = "{ \"totalSales\": 1000 }")
+//            )
+//    )
     @GetMapping("/api/sales?date=yesterday")
     public SalesResponse getYesterdaySales() {
         int totalSales = salesService.getYesterdayTotalSales();
