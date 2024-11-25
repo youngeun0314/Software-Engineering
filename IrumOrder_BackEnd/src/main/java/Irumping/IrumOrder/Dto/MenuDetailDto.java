1ecf1c91b12604a7b23df7d5bc46c7287ae8e10f
ej9374 <ej9374@naver.com>
2024-11-25 16:37:18 +0900

    docs: swagger 수정

--- IrumOrder_BackEnd/src/main/java/Irumping/IrumOrder/Dto/MenuDetailDto.java
+++ IrumOrder_BackEnd/src/main/java/Irumping/IrumOrder/Dto/MenuDetailDto.java
@@ -1,5 +1,6 @@
 package Irumping.IrumOrder.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,7 +8,8 @@ import lombok.Setter;
 @Setter
 public class MenuDetailDto {
 
-    private String useCup;         // 컵 사용 여부
+    @Schema(example="TakeOut")
+    private String useCup;     // 컵 사용 여부
     private Boolean addShot;        // 샷 추가 여부
     private Boolean addVanilla;     // 바닐라 추가 여부
     private Boolean addHazelnut;    // 헤이즐넛 추가 여부
--- IrumOrder_BackEnd/src/main/java/Irumping/IrumOrder/Dto/OrderRequestDto.java
+++ IrumOrder_BackEnd/src/main/java/Irumping/IrumOrder/Dto/OrderRequestDto.java
@@ -1,5 +1,6 @@
 package Irumping.IrumOrder.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.bind.DefaultValue;
@@ -14,6 +15,7 @@ public class OrderRequestDto {
     private int userId;
     private int totalPrice;
 
+    @Schema(example="01:20")
     private LocalTime pickUp;
 
 //    private boolean payment = false; // 결제 상태, 기본값은 false
