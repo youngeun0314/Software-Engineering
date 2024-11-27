c030e579577ed7e19f3ecda2d9cd4ffdf05aa522
ej9374 <ej9374@naver.com>
2024-11-27 18:19:38 +0900

    feat: token entity,repository 추가

---
+++ IrumOrder_BackEnd/src/main/java/Irumping/IrumOrder/entity/TokenEntity.java
@@ -0,0 +1,30 @@
+
+package Irumping.IrumOrder.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "token")
+public class TokenEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "token_id", nullable = false)
+    private Long tokenId;
+
+    @JoinColumn(name = "user_id", nullable = false)
+    private UserEntity user;
+
+    @Column(name = "fcmToken", nullable = false)
+    private String fcmToken;
+
+    public TokenEntity(UserEntity user, String fcmToken) {
+        this.user = user;
+        this.fcmToken = fcmToken;
+    }
+}
