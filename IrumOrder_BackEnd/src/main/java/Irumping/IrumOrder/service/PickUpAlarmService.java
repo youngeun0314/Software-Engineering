f1f87f0be15f752f6f7a96f4c30f33fbaece8cca
ej9374 <ej9374@naver.com>
2024-11-27 12:36:20 +0900

    chore: 민감정보 gitignore에 업데이트

--- IrumOrder_BackEnd/src/main/java/Irumping/IrumOrder/service/PickUpAlarmService.java
+++ IrumOrder_BackEnd/src/main/java/Irumping/IrumOrder/service/PickUpAlarmService.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 
+
 @Service
 @RequiredArgsConstructor
 public class PickUpAlarmService {
