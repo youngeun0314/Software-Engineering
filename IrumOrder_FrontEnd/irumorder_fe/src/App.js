67d5f3a2064c743424bae278f67c2d6a4f13f3d5
glebungle <cjy031212@gmail.com>
2024-11-29 16:07:41 +0900

    결제 완료 화면 및 링크 추가

--- IrumOrder_FrontEnd/irumorder_fe/src/App.js
+++ IrumOrder_FrontEnd/irumorder_fe/src/App.js
@@ -1,44 +1,17 @@
-import React from "react";
-import "./Payment.css";
+import React from 'react';
+import {HashRouter,Route, Routes} from 'react-router-dom';
+import Payment from './routes/Payment';
+import Paymentcomplete from './routes/Paymentcomplete';
 
-const PaymentScreen = () => {
-  return (
-    <div className="payment-container">
-      <header className="payment-header">
-        <button className="back-button">
-          <img
-          src={`${process.env.PUBLIC_URL}/ep_back.png`}
-          alt="go to back"
-          className="back-image"
-        /></button>
-        <div className="payment-text">
-          <p>결제</p>
-        </div>
-        
-      </header>
-      <div className="order-info">
-        <p className="order-title">주문 내역</p>
-        <p className="order-detail">(ICE) 아메리카노 1건</p>
-        <p className="pickup-time-title">픽업 시간</p>
-        <p className="pickup-time">바로 주문</p>
-        <p className="pickup-place-title">픽업 장소</p>
-        <p className="pickup-place">학생회관</p>
-      </div>
-      <div className="divider"></div>
-      <div className="payment-summary">
-        <p>결제 금액</p>
-        <p className="payment-amount">1,600원</p>
-      </div>
-      <div className="line-divider"></div>
-      <div className="payment-image-container">
-        <img
-          src={`${process.env.PUBLIC_URL}/Group 2.png`}
-          alt="pay로 결제하기"
-          className="payment-image"
-        />
-      </div>
-    </div>
-  );
-};
-
-export default PaymentScreen;
+function App(){
+  return(
+    <HashRouter>
+        <Routes>
+        <Route path="/payment" element={<Payment/>}/>
+        <Route path="/paymentcomplete" element={<Paymentcomplete/>}/>
+        </Routes>
+    </HashRouter>
+  )
+}
+ 
+export default App;
