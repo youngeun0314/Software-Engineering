a9d73f196a9dddcb54f078a5e2bc673a037e5954
nyungovo <nyungovo@gmail.com>
2024-11-30 22:10:14 +0900

    chore: unused import 삭제
    controller의 unused import 삭제
    
    issue #6

--- IrumOrder_BackEnd/src/main/java/Irumping/IrumOrder/controller/SalesController.java
+++ IrumOrder_BackEnd/src/main/java/Irumping/IrumOrder/controller/SalesController.java
@@ -2,9 +2,6 @@ package Irumping.IrumOrder.controller;
 
 import Irumping.IrumOrder.dto.SalesResponse;
 import Irumping.IrumOrder.service.SalesService;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.ExampleObject;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
