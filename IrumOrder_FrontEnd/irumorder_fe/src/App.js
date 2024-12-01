c70a4c27c7df94ab8b235e73f0eedd4b4dbdc37d
glebungle <cjy031212@gmail.com>
2024-12-02 06:01:29 +0900

    feat:장바구니 ui 추가

--- IrumOrder_FrontEnd/irumorder_fe/src/App.js
+++ IrumOrder_FrontEnd/irumorder_fe/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {HashRouter,Route, Routes} from 'react-router-dom';
+import Cart from './routes/Cart';
 import Payment from './routes/Payment';
 import Paymentcomplete from './routes/Paymentcomplete';
 
@@ -7,6 +8,7 @@ function App(){
   return(
     <HashRouter>
         <Routes>
+        <Route path="/cart" element={<Cart/>}/>
         <Route path="/payment" element={<Payment/>}/>
         <Route path="/paymentcomplete" element={<Paymentcomplete/>}/>
         </Routes>
