02859cdb6a785337f81aab9c7df5b5573f6e24bf
ej9374 <ej9374@naver.com>
2024-12-03 13:43:41 +0900

    chore: ì오류 수정

--- IrumOrder_BackEnd/src/main/java/Irumping/IrumOrder/service/OrderService.java
+++ IrumOrder_BackEnd/src/main/java/Irumping/IrumOrder/service/OrderService.java
@@ -18,8 +18,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.slf4j.Logger;
